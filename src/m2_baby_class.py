cafe5fbf9e2b5fa41d2d7e287d26831daef63f77
wilcoxeb <wilcoxeb@rose-hulman.edu>
2019-01-11 12:54:00 -0500

    m1 session 10

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Emily Wilcox.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -42,7 +42,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.
+# Done: 2.
 #
 #  Step 2a:  Implement a class called   Baby   that has a constructor method
 #            (__init__) and two other methods, as described below.
@@ -107,8 +107,25 @@ def main():
 #          """ Brief description of what an object of the class 'is'. """
 #
 ###############################################################################
+class Baby(object):
 
+    def __init__(self, baby_name):
+        self.babyname = baby_name
+        self.hours = 0
+        print('Hello baby {}'.format(baby_name))
 
+    def feed_baby(self):
+        print('Thank you for feeding baby {}'.format(self.babyname))
+        self.hours = 0
+
+    def hour_passes(self):
+        if self.hours == 0:
+            print('Baby {} is sleeping'.format(self.babyname))
+        if self.hours == 1:
+            print('Baby {} is awake. Time for food'.format(self.babyname))
+        if self.hours >= 2:
+            print('Baby {} is CRYING uncontrollably! Feed the Baby!'.format(self.babyname))
+        self.hours = self.hours + 1
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
--- src/m3_capital_t_class.py
+++ src/m3_capital_t_class.py
@@ -3,8 +3,8 @@ A   CapitalT   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Emily Wilcox.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
